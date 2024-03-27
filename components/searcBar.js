509f1f8161012de4107a8a81982b404da33880ca
Efe Eroğlu <210541019@firat.edu.tr>
2024-03-27 14:53:26 +0300

    Initial Commit

---
+++ components/searcBar.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function searcBar() {
+  return (
+    <View>
+      <Text>searcBar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/loginScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function loginScreen() {
+  return (
+    <View>
+      <Text>loginScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
