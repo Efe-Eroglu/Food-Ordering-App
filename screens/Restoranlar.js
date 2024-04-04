c7e37a53a2bc56bed2445ba08f4be7dde2ee06a9
Efe Eroğlu <210541019@firat.edu.tr>
2024-04-04 16:52:22 +0300

    Campaign 1 page update

--- screens/Restoranlar.js
+++ screens/Restoranlar.js
@@ -11,27 +11,32 @@ import React, { useEffect } from "react";
 import Bar from "../components/Bar";
 import { Entypo } from "@expo/vector-icons";
 import restaurants_data from "../data/restaurants_data";
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from "@expo/vector-icons";
 import FilterBar from "../components/FilterBar";
 
 const renderItem = ({ item }) => (
   <View>
-    <TouchableOpacity style={styles.cart} activeOpacity={0.8}>
+    <TouchableOpacity style={styles.cart} activeOpacity={0.9}>
       <View style={styles.leftSide}>
         <Image source={item.image} style={styles.image} resizeMode="stretch" />
       </View>
       <View style={styles.rightSide}>
-        <Text>{item.name}</Text>
-        <Text>
-          <Entypo name="star" size={18} color="yellow" /> {item.rating}
-        </Text>
-        <Text style={styles.delivery}>
-        <FontAwesome name="motorcycle" size={16} color="black" /> {item.delivery}<Text style={{fontSize:10}}>dk</Text>
-        </Text>
+        <Text style={styles.name}>{item.name}</Text>
+        <View style={styles.icons}>
+          <Text style={{marginRight:5}}>
+            <Entypo name="star" size={18} color="#edd142" />  {item.rating}
+          </Text>
+          <Text style={styles.delivery}>
+            <FontAwesome name="motorcycle" size={16} color="gray" />{" "}
+            {item.delivery}
+            <Text style={{ fontSize: 10 }}>dk</Text>
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   </View>
 );
+
 export default function Restoranlar() {
   useEffect(() => {
     StatusBar.setBackgroundColor("#ad3103");
@@ -63,25 +68,28 @@ const styles = StyleSheet.create({
   },
   cart: {
     width: 350,
-    height: 120,
+    height: 250,
     borderRadius: 10,
     backgroundColor: "#fff",
     marginBottom: 15,
-    flexDirection: "row",
     overflow: "hidden",
-    elevation: 10,
+    elevation: 1,
+    borderWidth: 1,
+    borderColor: "silver",
   },
   image: {
     width: "100%",
     height: "100%",
   },
   rightSide: {
-    flex: 4,
+    flex: 2,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    flexDirection: "row",
   },
   leftSide: {
-    flex: 3,
+    flex: 8,
   },
   title: {
     alignSelf: "flex-start",
@@ -90,7 +98,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontWeight: "bold",
   },
-  delivery:{
-    fontSize:14
-  }
+  delivery: {
+    fontSize: 14,
+    marginLeft: 20,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+  icons: {
+    alignItems: "flex-end",
+  },
 });
