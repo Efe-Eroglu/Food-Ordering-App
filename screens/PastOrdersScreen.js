d75a6d44bcb8890591149bb74fae8046e5b8f28e
Efe Eroğlu <210541019@firat.edu.tr>
2024-04-08 19:25:57 +0300

    Past Order Screen

--- screens/PastOrdersScreen.js
+++ screens/PastOrdersScreen.js
@@ -1,14 +1,20 @@
 import {
+  FlatList,
+  Image,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
+import { EvilIcons } from '@expo/vector-icons';
 import React, { useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import PastOrderBar from "../components/PastOrderBar";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import restaurants_data from "../data/restaurants_data";
 
 export default function PastOrdersScreen() {
   const navigation = useNavigation();
@@ -18,17 +24,90 @@ export default function PastOrdersScreen() {
     StatusBar.setBarStyle("light-content");
   }, []);
 
+  const renderItem = ({ item }) => (
+    <View>
+      <View style={styles.card}>
+        <View style={styles.leftSide}>
+          <Image
+            source={item.image}
+            style={styles.image}
+            resizeMode="stretch"
+          />
+        </View>
+        <View style={styles.rightSide}>
+          <View style={styles.content}>
+            <Text>{item.name}</Text>
+            <Text style={{ marginRight: 5, fontSize:12 }}>
+              <Entypo name="star" size={18} color="#edd142" /> {item.rating}
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+            <Text style={styles.buttonText}>Değerlendir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
   return (
-    <View style={styles.container}>
+    <View style={styles.outcontainer}>
       <PastOrderBar />
+
+      <View style={styles.container}>
+        <FlatList
+          data={restaurants_data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outcontainer: {
     flex: 1,
-    backgroundColor: "silver",
+    backgroundColor: "#fff",
     alignItems: "center",
+  },
+  container: {
+    marginTop: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  card: {
+    width: 350,
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    marginBottom: 15,
+    overflow: "hidden",
+    elevation: 1,
+    borderWidth: 1,
+    borderColor: "rgba(220,220,220,0.5)",
+    flexDirection: "row",
+  },
+  leftSide: {
+    flex: 4,
+  },
+  rightSide: {
+    flex: 6,
+    margin: 10,
+  },
+  button: {
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop:15,
+    justifyContent:"center",
+    flexDirection:"row",
+    alignItems:"center",
+    borderColor:"#6e2305",
+    backgroundColor:"rgba(110, 35, 5,0.1)"
+},
+  buttonText: {
+    textAlign:"center",
+    padding:1.2
   },
 });
